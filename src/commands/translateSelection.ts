f6d5062748fbed37914cf19d5b44277058e523dc
clockblocker <asde3211q@gmail.com>
2025-11-29 12:28:07 +0100

    add new api to translateSelection

--- src/commands/translateSelection.ts
+++ src/commands/translateSelection.ts
@@ -1,5 +1,7 @@
 import { type Editor, Notice } from "obsidian";
 import type TextEaterPlugin from "../main";
+import { prompts } from "prompts";
+import z from "zod";
 
 export default async function translateSelection(
 	plugin: TextEaterPlugin,
@@ -8,7 +10,13 @@ export default async function translateSelection(
 ) {
 	try {
 		const cursor = editor.getCursor();
-		const response = await plugin.apiService.translateText(selection);
+		const response = await plugin.newApiService.generate({
+			systemPrompt: prompts.translate_de_to_eng,
+			userInput: selection,
+			schema: z.string(),
+			withCache: false,
+		});
+		
 		if (response) {
 			editor.replaceSelection(selection + "\n\n" + response + "\n");
 			editor.setCursor({
