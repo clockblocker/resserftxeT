8dc23eabde4b8a1da68a8f66fb2e13273592d6b8
clockblocker <asde3211q@gmail.com>
2025-11-30 07:53:02 +0100

    final fixess

--- src/commands/getInfinitive.ts
+++ src/commands/getInfinitive.ts
@@ -1,14 +1,14 @@
 import { type Editor, Notice, type TFile } from "obsidian";
-import type TextEaterPlugin from "../main";
-import z from "zod";
 import { prompts } from "prompts";
+import z from "zod";
+import type TextEaterPlugin from "../main";
 
 export async function getInfinitiveFromFilename(
 	plugin: TextEaterPlugin,
 	file: TFile,
 ) {
 	const text = file.basename;
-	const res = await getInfinitive(plugin, text)
+	const res = await getInfinitive(plugin, text);
 
 	await plugin.fileService.writeToOpenedFile(` ${res} \n`);
 }
@@ -18,22 +18,24 @@ export async function getInfinitiveFromSelection(
 	editor: Editor,
 	text: string,
 ) {
-	const res = await getInfinitive(plugin, text)
+	const res = await getInfinitive(plugin, text);
 
 	await navigator.clipboard.writeText(`${res}`);
 	editor.replaceSelection(`${res}`);
 }
 
-
-async function getInfinitive(
-	plugin: TextEaterPlugin,
-	text: string,
-) {
-	const hebPartsSchema = z.array(z.array(z.object({ surf: z.string(), lem: z.string() })));
+async function getInfinitive(plugin: TextEaterPlugin, text: string) {
+	const hebPartsSchema = z.array(
+		z.array(z.object({ surf: z.string(), lem: z.string() })),
+	);
 	type HebParts = z.infer<typeof hebPartsSchema>;
 
 	const makeMdForHebParts = (parts: HebParts): string => {
-		return parts.map(word => word.map(part => `[[${part.lem}|${part.surf}]]`).join("|")).join(" ");
+		return parts
+			.map((word) =>
+				word.map((part) => `[[${part.lem}|${part.surf}]]`).join("|"),
+			)
+			.join(" ");
 	};
 
 	try {
@@ -43,14 +45,22 @@ async function getInfinitive(
 			withCache: false,
 		});
 
-		const response = rawResponse.replaceAll("javascript", "").replaceAll("```", "").replaceAll("\n", "").replace(/^\n+/, "").replaceAll('json', '').trim()
+		const response = rawResponse
+			.replaceAll("javascript", "")
+			.replaceAll("```", "")
+			.replaceAll("\n", "")
+			.replace(/^\n+/, "")
+			.replaceAll("json", "")
+			.trim();
 		const parsed = JSON.parse(response);
 
 		const parsedResponse = hebPartsSchema.safeParse(parsed);
+
+		console.log(parsedResponse);
 		if (!parsedResponse.success) {
 			new Notice(`Error: ${parsedResponse.error.message}`);
 			return;
-		}	
+		}
 
 		const parts = parsedResponse.data;
 		return makeMdForHebParts(parts);
@@ -58,4 +68,3 @@ async function getInfinitive(
 		new Notice(`Error: ${error.message}`);
 	}
 }
-
